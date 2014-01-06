8ad890b644958c2a016bb6289398152fdad3e007
DaveBueds <davebueds@MacBook-Pro-van-David.local>
2014-01-06 10:33:43 +0100

    Aanmaken MainPandapp
    /

---
+++ src/be/pandapp/ehb/MainPandapp.java
@@ -0,0 +1,24 @@
+package be.pandapp.ehb;
+
+/**
+ * @author 
+ *
+ */
+public class MainPandapp {
+
+	/**
+	 * 
+	 */
+	public MainPandapp() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
