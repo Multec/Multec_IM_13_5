60fffa4299c80bc5199d9b036ee23df308d66433
DaveBueds <davebueds@MacBook-Pro-van-David.local>
2014-01-06 13:42:03 +0100

    Aanpassen NewsTile
    /

--- src/be/pandapp/ehb/NewsTile.java
+++ src/be/pandapp/ehb/NewsTile.java
@@ -5,28 +5,64 @@ import processing.core.PImage;
 
 public class NewsTile extends Shape {
 	
-	public int posX;
-	public int posY;
-	//public String imagePath;
 	public PImage image;
 	
+	private float newsTile;
 
-	public NewsTile(float posX, float posY, float posZ, int fillColor, PImage image) {
+	private float width;
+	private float height;
+	private float depth;
+	
+	public NewsTile(float posX, float posY, float posZ, int fillColor, PImage image, float width, float height, float depth) {
 		super(posX, posY, posZ, fillColor);
 		this.image = image;
+		
+		this.width = width;
+		this.height = height;
+		this.depth = depth;
 	}
 	
-	public void setup() {
-		
+	public PImage getImage() {
+		return image;
 	}
 
-	public void draw() {
-		
+	public void setImage(PImage image) {
+		this.image = image;
+	}
+
+	public float getWidth() {
+		return width;
+	}
+
+	public void setWidth(float width) {
+		this.width = width;
+	}
+
+	public float getHeight() {
+		return height;
+	}
+
+	public void setHeight(float height) {
+		this.height = height;
+	}
+
+	public float getDepth() {
+		return depth;
+	}
+
+	public void setDepth(float depth) {
+		this.depth = depth;
 	}
 
 	@Override
 	public void draw(PApplet app) {
-		// TODO Auto-generated method stub
+		app.fill(fillColor);
+		app.stroke(strokeColor);
+		app.strokeWeight(strokeWeight);
+		
+		app.pushMatrix();
+		
+		app.popMatrix();
 		
 	}
 }
