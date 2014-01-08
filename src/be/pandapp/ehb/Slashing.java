4ad46efc0b2496f6ae20f929e67b7a89f25b1411
AdrienEHB <adrien.schautteet@student.ehb.be>
2014-01-08 14:05:30 +0100

    Slashtag 3.0

--- src/be/pandapp/ehb/Slashing.java
+++ src/be/pandapp/ehb/Slashing.java
@@ -7,48 +7,60 @@ public class Slashing extends SlashTag {
 	boolean mouseAfterClick;
 	boolean outOfPlace;
 	int lastMouseX;
-	PApplet app;
+
 
 	public Slashing(float x, float y, float z, int fillColor, float scale,
-			PShape headAndFoot, PShape middle, PShape text, PApplet app) {
+			PShape headAndFoot, PShape middle, PShape text) {
 		super(x, y, z, fillColor, scale, headAndFoot, middle, text);
-		this.app = app;
 		// TODO Auto-generated constructor stub
 
 	}
 
-	int mouseX2 = (int) (app.mouseX * 0.245);
-	int lastMouseX2 = (int) (lastMouseX * 0.245);
-
-	public void draw() {
-		if (app.mouseX >= 410 && app.mousePressed) {
-
+	public void draw(PApplet app) {
+		
+		app.ellipse(app.mouseX, app.mouseY, 20, 20);
+		int mouseX2 = (int) (app.mouseX * 0.245);
+		int lastMouseX2 = (int) (lastMouseX * 0.245);
+		
+		if (app.mouseX >= (x + 10) && app.mousePressed) {
+			System.out.println("test1");
 			outOfPlace = true;
 
-			app.shape(middle, app.mouseX, 492 - (mouseX2));
+			app.shape(middle, app.mouseX, (x + 92) - (mouseX2));
 		}
 
-		else if (lastMouseX < 420) {
+		else if (lastMouseX < (x + 20)) {
 			outOfPlace = false;
 			mouseAfterClick = false;
+			System.out.println("test2");
 		}
 
 		if (outOfPlace && mouseAfterClick && app.mousePressed == false) {
 
-			app.shape(middle, lastMouseX, 492 - (lastMouseX2));
+			app.shape(middle, lastMouseX, (x + 92) - (lastMouseX2));
 			lastMouseX -= 15;
+			System.out.println("test3");
 		}
 
 		else if (outOfPlace == false) {
+			//System.out.println("test5");
 
 		}
+		app.pushMatrix();
+		app.translate(x, y);
+		app.scale(scale);
+		app.shape(headAndFoot);
+		app.shape(middle);
+		app.shape(text);
+		app.popMatrix();
 
 	}
 
-	public void mouseReleased() {
+	public void mouseReleased(PApplet app) {
 
 		mouseAfterClick = true;
 		lastMouseX = app.mouseX;
+		System.out.println("test6");
 	}
 
 }
