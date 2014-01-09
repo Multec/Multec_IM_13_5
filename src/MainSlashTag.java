c5250c45a31dd68aad00bee41536fb5606a356b5
AdrienEHB <adrien.schautteet@student.ehb.be>
2014-01-09 15:34:28 +0100

    SlashtagOverAndOut

--- src/MainSlashTag.java
+++ src/MainSlashTag.java
@@ -22,12 +22,13 @@ public class MainSlashTag extends PApplet {
 
 	private int aantal = 15;
 	Slashing Slashing[] = new Slashing[aantal];
+	PShape[] bodies = new PShape[4];
+	PShape[] middles = new PShape[4];
 	private PShape headAndFoot;
 	private PShape middle;
 	private PShape text;
-
-	// ---
-
+	private boolean bool;
+	private Integer index;
 	boolean mouseAfterClick;
 	boolean outOfPlace;
 	int lastMouseX;
@@ -36,23 +37,38 @@ public class MainSlashTag extends PApplet {
 		size(1280, 800);
 		background(0);
 		smooth();
-		headAndFoot = loadShape("../images/SlashTagProcessingHeadFoot.svg");
-		middle = loadShape("../images/SlashTagProcessingMiddle.svg");
-		text = loadShape("../images/SlashTagProcessingText.svg");
-		text.disableStyle();
+		bool = false;
 		outOfPlace = false;
+		index = null;
 	}
 
 	public void draw() {
-		background(0);
+		bodies[0] = loadShape("../images/SlashTagProcessingHeadFoot.svg");
+		bodies[1] = loadShape("../images/bodyBlue.svg");
+		bodies[2] = loadShape("../images/bodyBlueDark.svg");
+		bodies[3] = loadShape("../images/bodyRed.svg");
+		middles[0] = loadShape("../images/SlashTagProcessingMiddle.svg");
+		middles[1] = loadShape("../images/middleBlue.svg");
+		middles[2] = loadShape("../images/middleBlueDark.svg");
+		middles[3] = loadShape("../images/middleRed.svg");
 		
+		
+		/*headAndFoot = loadShape("../images/SlashTagProcessingHeadFoot.svg");
+		middle = loadShape("../images/SlashTagProcessingMiddle.svg");*/
+		text = loadShape("../images/SlashTagProcessingText.svg");
+		background(0);
+
 		if (frameCount <= 1) {
 			if (Slashing.length >= 0) {
 				for (int i = 0; i < this.aantal; i++) {
+					float randomColor = random(0, 4);
+					headAndFoot = bodies[(int) randomColor];
+					middle = middles[(int) randomColor];
 
 					if (i == 0) {
 						Slashing[i] = new Slashing(random(0, 100), random(0,
-								100), 0, 255, 1, headAndFoot, middle, text);
+								100), 0, 255, 1, headAndFoot, middle, text,
+								bool);
 
 					}
 
@@ -60,13 +76,14 @@ public class MainSlashTag extends PApplet {
 						Slashing[i] = new Slashing(random(
 								Slashing[i - 1].getX() + 150,
 								Slashing[i - 1].getX() + 275), random(0, 100),
-								0, 255, 1, headAndFoot, middle, text);
+								0, 255, 1, headAndFoot, middle, text, bool);
 
 					}
 
 					if (i == 5) {
 						Slashing[i] = new Slashing(random(0, 100), random(250,
-								350), 0, 255, 1, headAndFoot, middle, text);
+								350), 0, 255, 1, headAndFoot, middle, text,
+								bool);
 
 					}
 
@@ -76,16 +93,13 @@ public class MainSlashTag extends PApplet {
 								Slashing[i - 1].getX() + 150,
 								Slashing[i - 1].getX() + 275),
 								random(250, 350), 0, 255, 1, headAndFoot,
-								middle, text);
-
-						// System.out.println(Slashing[i].getX());
-
+								middle, text, bool);
 					}
 
 					if (i == 10) {
 						Slashing[i] = new Slashing(random(0, 100), random(500,
-								600), 0, 255, 1, headAndFoot, middle, text);
-
+								600), 0, 255, 1, headAndFoot, middle, text,
+								bool);
 					}
 
 					if (i <= 15 && i > 10) {
@@ -93,10 +107,7 @@ public class MainSlashTag extends PApplet {
 								Slashing[i - 1].getX() + 150,
 								Slashing[i - 1].getX() + 275),
 								random(500, 600), 0, 255, 1, headAndFoot,
-								middle, text);
-
-						// System.out.println(Slashing[i].getX());
-
+								middle, text, bool);
 					}
 
 				}
@@ -104,11 +115,41 @@ public class MainSlashTag extends PApplet {
 		}
 		if (Slashing.length == 15) {
 			for (int i = 0; i < Slashing.length; i++) {
+
 				Slashing[i].draw(this);
-				if (!mousePressed) {
-					Slashing[i].mouseReleased(this);
+				// als de muispositie groter is dan de xwaarde en
+				// ywaarde van slashtag en kleiner is dan diezelfde
+				// waarden + 200
+
+				if (mouseX > Slashing[i].getX()
+						&& mouseX < Slashing[i].getX() + 200
+						&& mouseY > Slashing[i].getY()
+						&& mouseY < Slashing[i].getY() + 200 && mousePressed) {
+					
+					if (index == null) {
+						index = i;
+
+					}// huidige i waarde van slashing object
+						// opslaan in index
+
+					else {
+						// index = null;
+
+					}
+					Slashing[index].setDraggable(true);
 				}
-				
+
+				else if (!mousePressed) {
+					Slashing[i].mouseReleased(this);
+					index = null;
+
+				}
+				// System.out.println(mousePressed + "de mousepressed");
+
+				else {
+					Slashing[i].setDraggable(false);
+				}
+
 			}
 			// System.out.println(Slashing[1].getX());
 		}
--- src/be/pandapp/ehb/Slashing.java
+++ src/be/pandapp/ehb/Slashing.java
@@ -1,76 +1,77 @@
 package be.pandapp.ehb;
+
 import processing.core.*;
+
 public class Slashing extends SlashTag {
-	
+
 	boolean mouseAfterClick;
 	boolean outOfPlace;
 	int lastMouseX;
-	
-
+	boolean draggable;
 
 	public Slashing(float x, float y, float z, int fillColor, float scale,
-			PShape headAndFoot, PShape middle, PShape text) {
+			PShape headAndFoot, PShape middle, PShape text, boolean draggable) {
 		super(x, y, z, fillColor, scale, headAndFoot, middle, text);
-		//this.middle = middle;
-		headAndFoot.disableStyle();
-		middle.disableStyle();	
-		// TODO Auto-generated constructor stub
+		this.draggable = draggable;
+
 	}
 
-	 public void draw(PApplet app) {
-		 
-		app.fill(123, 123, 100);
+	public void draw(PApplet app) {
+		float localX = app.mouseX - x;
 		app.ellipse(app.mouseX, app.mouseY, 20, 20);
 		app.pushMatrix();
 		app.translate(x, y);
 		app.scale(scale);
 		app.shape(headAndFoot);
-		//app.shape(middle);
-		app.shape(text);
 		app.popMatrix();
-		
-		int mouseX2 = (int) (app.mouseX * 0.245);
-		int lastMouseX2 = (int) (lastMouseX * 0.245);
-		
-		if (app.mouseX >= (x + 10) && app.mousePressed) {
-			System.out.println("test1");
-			outOfPlace = true;
-			app.shape(middle, app.mouseX, (x + 92) - (mouseX2));
-		}
 
-		else if (lastMouseX < (x + 20)) {
-			outOfPlace = false;
-			mouseAfterClick = false;
-			//System.out.println("test2");
-		}
-		if (outOfPlace && mouseAfterClick && app.mousePressed == false) {
+		if (draggable == true) {
 
-			app.shape(middle, lastMouseX, (x + 92) - (lastMouseX2));
-			lastMouseX -= 15;
-			//System.out.println("test3");
-		}
-		else if (outOfPlace == false) {
-			//System.out.println("test5");
+			if (app.mouseX >= x && app.mousePressed) {
+				outOfPlace = true;
+				app.shape(middle, app.mouseX, (int) (y - localX * 0.245));
+				app.shape(text, app.mouseX, (int) (y - localX * 0.245));
 
+			}
+
+			else if (!app.mousePressed) {
+			/*	app.shape(middle, x, y);
+				app.shape(text, x, y);*/
+
+			}
+
+			else if (lastMouseX < app.mouseX) {
+				outOfPlace = false;
+				mouseAfterClick = false;
+				System.out.println("outofplace");
+
+			}
+
+			if (outOfPlace && mouseAfterClick && app.mousePressed == false) {
+				/*app.shape(middle, x, y);
+				app.shape(text, x, y);*/
+				lastMouseX -= 15;
+
+			}
+		}
+		else {
+			app.shape(middle, x, y);
+			app.shape(text, x, y);
 		}
 
 	}
-	
-	 
-	 
-	public void mouseReleased(PApplet app) {
 
+	public void mouseReleased(PApplet app) {
 		mouseAfterClick = true;
 		lastMouseX = app.mouseX;
-		//System.out.println("test6");
+
 	}
-	
-	/*public void drag(PApplet app) {
-		if (app.mouseX >= x && app.mouseX + 200 < x && app.mouseX >= y && app.mouseX + 200 < y) {
-			
-		}
 
-	}*/
+	public boolean isDraggable() {
+		return draggable;
+	}
 
-	
+	public void setDraggable(boolean draggable) {
+		this.draggable = draggable;
+	}
 }
